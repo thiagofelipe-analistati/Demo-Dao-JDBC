098881bbfb2ad8db7f6a5480464b3d98ffe93279
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-03-19 16:38:14 -0300

    Criação dos entidade de classe Departamento

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import model.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento( 1, "Livros");
+	
+		System.out.println(obj);
+
+	}
+
+}
