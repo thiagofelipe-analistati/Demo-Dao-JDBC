8935328b067d9da070401dbc2589bdb6e554a8eb
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-03-19 17:06:29 -0300

    Criação da entidade vendedor

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,14 +1,23 @@
 package aplicacao;
 
+import java.util.Date;
+
 import model.entidades.Departamento;
+import model.entidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Departamento obj = new Departamento( 1, "Livros");
+		Vendedor obj1 = new Vendedor(1, "Thiago", "fel_thiago@outlook.com", new Date(), 10000.0, obj);
+		
+		System.out.println();
 	
 		System.out.println(obj);
+
+		System.out.println(obj1);
+
 
 	}
 
